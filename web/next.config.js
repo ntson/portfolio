54e8b5164c2eddfd0527b184c66b0e763e3af921
Nguyen Son <ngso.ts@gmail.com>
2022-01-15 21:47:03 +0700

    next.js redirects

--- web/next.config.js
+++ web/next.config.js
@@ -3,4 +3,13 @@ module.exports = {
     domains: ['avatars.githubusercontent.com', 'cdn.sanity.io'],
   },
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://ntson-portfolio-studio.sanity.studio',
+        permanent: true,
+      },
+    ];
+  },
 };
