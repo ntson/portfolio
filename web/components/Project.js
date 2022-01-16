cb486d9c81df81133311d36ef89c7821cec52ff9
Nguyen Son <ngso.ts@gmail.com>
2022-01-16 10:55:00 +0700

    fix image witdth

--- web/components/Project.js
+++ web/components/Project.js
@@ -3,19 +3,19 @@ import ProjectLink from './ProjectLink';
 
 const Project = ({ project }) => {
   return (
-    <article className="flex flex-col gap-2 sm:flex-row sm:items-center sm:gap-10">
-      <div>
+    <article className="flex flex-col gap-2 md:flex-row md:items-center md:gap-10">
+      <div className="w-full md:w-1/2">
         <h2 className="font-bold text-xl mb-3">{project.projectName}</h2>
         <Image
-          className="rounded-md"
           src={project.projectImage.asset.url}
           alt={`${project.projectName} screenshot`}
           width={500}
-          height={260}
+          height={300}
+          className="rounded-md"
         />
       </div>
 
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 md:w-1/2">
         <p>{project.description}</p>
         <ProjectLink url={project.githubUrl}>GitHub</ProjectLink>
         <ProjectLink url={project.homepageUrl}>Live</ProjectLink>
